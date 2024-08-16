dccbb5df2808a3478d7ece8ba97563fa12816d85
bloo <nexushellstein@gmail.com>
2024-08-16 17:28:08 +0800

    Fixed code for matrix field

--- pgD2/Simulations/sim1.py
+++ pgD2/Simulations/sim1.py
@@ -17,19 +17,15 @@ g1.set_window()
 ins, dic = g1.Loop(g1), d.Dictionary()
 
 #instantiate matrix
-m1 = m.Matrix(g1, pg, d, dic.color['WHITE'], dic.color['BLUE'])
+m1 = m.Matrix(g1, pg, d, dic.color['WHITE'], dic.color['BLACK'])
 
 #game loop:
-i = 1
 while g1.run:
     ins.set_loop()
     ins.set_events(None)
-    ins.set_background(dic.color['BLUE'])
+    ins.set_background(dic.color['BLACK'])
     #Counter
     pg.time.delay(500)
-    m1.paint_matrix_field(g1.win_center, 50, i, "HELLO WORLD")
-    i += 1
-    if i > 11:
-        i = 1
+    m1.paint_matrix_field(g1.win_center, 50, "HELLO WORLD")
     ins.update_display()
 pg.quit()
