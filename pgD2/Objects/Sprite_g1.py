73fec1d1dcdaba75bd76f34d647b4013b33456fd
bloo <nexushellstein@gmail.com>
2024-08-17 11:40:59 +0800

    re-labeling
    arranged the code and provided more comments

--- pgD2/Objects/Sprite_g1.py
+++ pgD2/Objects/Sprite_g1.py
@@ -4,7 +4,8 @@ pyg.init()
 class Sprite_g1(object):
     RECT, CIRC = 0, 1
     def __init__(self, game, center, size=(10,10), vel=(0,0), accel=(0,0), shape=RECT, color=(255,255,255)):
-        s, g = self, game                                                  #initializing parameter-based properties
+        #initializing parameter-based properties
+        s, g = self, game                                                  
         s.game, s.center, s.size, s.vel, s.accel, s.shape, s.color = game, center, size, vel, accel, shape, color
         #parameter-derived properties:
         s.pos = (s.size[0] - size[0] / 2, s.size[1] - size[1] / 2)
@@ -19,8 +20,10 @@ class Sprite_g1(object):
         s.en_prop_val = [1.0, 0.5, -0.25, 0, 1.0]                               #initial values
         for i, val in s.en_prop, s.en_prop_val:                                 #assigning initial values as default properties
             s.en_prop[i] = val
+        #object detection
+        s.platforms = []
 
-    def actions():
+    def actions():                  #to-do: 4, to test:0
         def move(keys):             #to-do
             return
         def jump(keys, strength):   #to-do
@@ -32,26 +35,32 @@ class Sprite_g1(object):
 
     def update(self):               #to-do: 3   to test: 2
         grav, fric, winds, col, bounce = self.en_prop       #senses the environment
-        if self.gravity:            #to test
-            Sprite_g1.grav_logic(grav)
-        if self.friction:           #to test
-            Sprite_g1.fric_logic(fric)
-        if self.wind:               #to-do
-            Sprite_g1.
-        if self.collision:          #to-do
-            return
-        if self.bounce:             #to-do
-            return
+        Sprite_g1.environment_logics(grav, fric, winds, col, bounce)
         Sprite_g1.draw_sprite()
 
     def draw_sprite(self):          #to test
         if self.shape == Sprite_g1.RECT:
             pyg.draw.rect(self.game.win, (self.pos[0], self.pos[1], self.size[0], self.size[1]), self.color)
+    def interact(self, platforms):  #to test
+        self.platforms = platforms
     def get_direction(self):        #to-do: get the net velocity and return a tuple of the direction
         return
     def is_grounded(self):          #to-do: detect if the sprite is on the ground
         return
 
+    def environment_logics(self, grav, fric, winds, col, bounce):
+        if self.gravity:            #to test
+            Sprite_g1.grav_logic(grav)
+        if self.friction:           #to test
+            Sprite_g1.fric_logic(fric)
+        if self.wind:               #to-test
+            Sprite_g1.wind_logic(winds)
+        if self.collision:          #to-test
+            Sprite_g1.col_logic(col)
+        if self.bounce:             #to-do
+                return
+
+    #environment_logics sub-methods
     def grav_logic(self, grav):     #to test
         self.accel[1] += grav
         self.vel[1] += self.accel[1]
@@ -63,7 +72,8 @@ class Sprite_g1(object):
             self.accel[0] = fric * self.accel[0]
             self.vel[0] += fric
     def wind_logic(self, wind):     #to-do
-        return
+        self.accel[0] += wind
+        self.vel[0] += self.accel[0]
     def col_logic(self, col):       #to-do
         return
     def bounce_logic(self, bounce): #to-do
--- pgD2/Simulations/sim2.py
+++ pgD2/Simulations/sim2.py
@@ -10,3 +10,22 @@ import Tools.Game as g
 import Tools.Dictionary as d
 import Objects.Sprite_g1 as s1
 
+#game object
+g1 = g.Game(pg)
+g1.set_window()
+
+#for less coding space
+loop, dic = g1.Loop(), d.Dictionary()
+
+#in-game objects
+
+
+
+#game loop
+while g1.run:
+    loop.set_loop()
+    loop.set_events()
+    loop.set_background(dic.color[dic.DARK_GREY])
+    #insert methods here
+    loop.update_display()
+pg.quit()
