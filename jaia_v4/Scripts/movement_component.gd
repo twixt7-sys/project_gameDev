000ad261b9508c07f8b773b790b04be3a272dd85
bloo <nexushellstein@gmail.com>
2025-04-16 15:06:51 +0800

    implemented rolling animation via animation tree

--- jaia_v4/Scripts/movement_component.gd
+++ jaia_v4/Scripts/movement_component.gd
@@ -5,7 +5,6 @@ extends Node2D
 @export var STAMINA: StaminaComponent
 
 @export var BODY : CharacterBody2D
-@export var SPRITE : AnimatedSprite2D
 @export var MOVEMENT_SPEED := 10.0
 @export var SPRINT_POWER := 1.8
 @export var BACKSTEP_POWER := 2
@@ -29,25 +28,6 @@ func move(delta: float, direction: Vector2, run: bool) -> void:
 	if STAMINA and run and direction != V_zero:
 		if not run_on_cooldown:
 			STAMINA.consume(1.2)
-
-	#flip_h(can_flip)
-	#animate(run, direction)
-
-func flip_h(active: bool):
-		if BODY.velocity.x < 0 and active:
-			SPRITE.flip_h = true
-		elif BODY.velocity.x > 0:
-			SPRITE.flip_h = false
-
-func animate(run_animation: bool, direction: Vector2):
-	if is_dashing:
-		SPRITE.play("roll")
-	elif run_animation and direction and not run_on_cooldown:
-		SPRITE.play("run")
-	elif direction:
-		SPRITE.play("walk")
-	else:
-		SPRITE.play("idle")
 
 func dash():
 	if STAMINA.stamina > 15:
--- jaia_v4/Scripts/player.gd
+++ jaia_v4/Scripts/player.gd
@@ -4,14 +4,12 @@ extends CharacterBody2D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var movement: MovementComponent = $"Movement Component"
 @onready var atk: Attack = $Attack
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var action: Action = $Action
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var dir: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
-	#animation_machine.add_state("roll", "is_rolling", func(): return movement.can_roll, func(): print("Roll done."))
 	animation_tree.active = true
 
 func _process(delta: float) -> void:
@@ -22,7 +20,6 @@ func _physics_process(delta: float) -> void:
 	attack(Input.is_action_just_pressed("slash"))
 	backstep(Input.is_action_just_pressed("backstep"))
 	roll(Input.is_action_just_pressed("roll"))
-	#animation_machine.update()
 
 func move(delta: float, run: bool) -> void:
 	action.action("move", 0.00001, true, func():
@@ -35,7 +32,7 @@ func attack(cond: bool):
 	var on_start = func():
 		print("Player Attacked.")
 		atk.origin = global_position
-	action.action("attack", get_anim_length("attack"), true, on_start)
+	action.action("attack", 0.5, true, on_start)
 
 func roll(cond: bool):
 	if not cond: return
@@ -47,37 +44,38 @@ func roll(cond: bool):
 	var on_end = func():
 		movement.can_flip = true
 		movement.can_roll = true
-	action.action("roll", get_anim_length("roll"), true, on_start, on_end)
+	action.action("roll", 0.5, true, on_start, on_end)
 
 func backstep(cond: bool):
 	if not cond: return
 	var on_start = func():
 		print("Player Backstepped.")
 		movement.can_flip = false
-		sprite.play("roll")
 		movement.backstep()
 	var on_end = func():
 		movement.can_flip = true
-	action.action("backstep", get_anim_length("roll"), true, on_start, on_end)
-
-func get_anim_length(anim_name: String) -> float:
-	if animation_tree and animation_tree.has_animation(anim_name):
-		return animation_tree.get_animation(anim_name).length
-	elif animation_player and animation_player.has_animation(anim_name):
-		return animation_player.get_animation(anim_name).length
-	else:
-		return 0.5  # fallback default
+	action.action("backstep", 0.5, true, on_start, on_end)
 
 func update_animation_parameters():
-	if (velocity == Vector2.ZERO):
-		animation_tree["parameters/conditions/idle"] = true
-		animation_tree["parameters/conditions/is_moving"] = false
-	else:
-		animation_tree["parameters/conditions/idle"] = false
-		animation_tree["parameters/conditions/is_moving"] = true
 
 	animation_tree["parameters/conditions/attack"] = true if Input.is_action_just_pressed("slash") else false
-	
-	if (dir != Vector2.ZERO):
-		animation_tree["parameters/idle/blend_position"] = dir
-		animation_tree["parameters/walk/blend_position"] = dir
+	animation_tree["parameters/conditions/is_rolling"] = true if Input.is_action_pressed("roll") else false
+
+	if animation_tree["parameters/conditions/is_rolling"]:
+		return
+
+	if Input.is_action_pressed("sprint") and velocity != Vector2.ZERO:
+		animation_tree["parameters/conditions/is_running"] = true
+		animation_tree["parameters/conditions/is_moving"] = false
+		animation_tree["parameters/conditions/idle"] = false
+	elif velocity != Vector2.ZERO:
+		animation_tree["parameters/conditions/is_running"] = false
+		animation_tree["parameters/conditions/is_moving"] = true
+		animation_tree["parameters/conditions/idle"] = false
+	else:
+		animation_tree["parameters/conditions/is_running"] = false
+		animation_tree["parameters/conditions/is_moving"] = false
+		animation_tree["parameters/conditions/idle"] = true
+
+	if dir != Vector2.ZERO: for x in ["idle", "walk", "run", "roll/BlendSpace2D"]:
+		animation_tree["parameters/%s/blend_position" % x] = dir
