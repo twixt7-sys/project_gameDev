fd902d929a357d968a797529e9919a44aad77bb4
bloo <nexushellstein@gmail.com>
2025-04-20 01:59:01 +0800

    added shadows and a new character

---
+++ jaia_v4/Scripts/GUI.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	pass
--- jaia_v4/Scripts/game.gd
+++ jaia_v4/Scripts/game.gd
@@ -2,12 +2,17 @@ extends Node
 
 @onready var player: Player = $Entities/Player
 @onready var slimes: Array = []
+@onready var camera: Camera2D = $Entities/Camera2D2
+@onready var gui: Control = $ControlParent/GUI
 
 func _ready() -> void:
 	for entity in $Entities.get_children():
 		if entity is BlueSlime:
 			slimes.append(entity)
 			entity.connect("patrol_area_exited", _on_patrol_area_exited)
+
+func _process(delta: float) -> void:
+	camera.position = player.position
 
 func _physics_process(delta: float) -> void:
 	for slime in slimes:
