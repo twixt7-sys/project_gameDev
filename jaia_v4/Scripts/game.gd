8b6f65be7e5f65e1d8aa63396cbc3ce8178112a2
bloo <nexushellstein@gmail.com>
2025-04-20 22:26:45 +0800

    initialized game interface

--- jaia_v4/Scripts/game.gd
+++ jaia_v4/Scripts/game.gd
@@ -3,16 +3,13 @@ extends Node
 @onready var player: Player = $Entities/Player
 @onready var slimes: Array = []
 @onready var gui: Control = $ControlParent/GUI
-@onready var camera: Camera2D = $Entities/Player/Camera2D2
+@onready var camera: Camera2D = $Entities/Player/Camera2D
 
 func _ready() -> void:
 	for entity in $Entities.get_children():
 		if entity is BlueSlime:
 			slimes.append(entity)
 			entity.connect("patrol_area_exited", _on_patrol_area_exited)
-
-func _process(delta: float) -> void:
-	camera.position = player.position
 
 func _physics_process(delta: float) -> void:
 	for slime in slimes:
