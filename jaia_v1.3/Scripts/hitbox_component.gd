427eaa133ac2b4e0b7c8c2eac084556ed4d5f0ac
bloo <nexushellstein@gmail.com>
2025-07-02 23:03:10 +0800

    removed project JAIA

--- jaia_v1.3/Scripts/hitbox_component.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-
-func _ready() -> void:
-	pass
-
-func _process(delta: float) -> void:
-	pass
--- jaia_v4/Scripts/GUI.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-func _process(delta: float) -> void:
-	pass
